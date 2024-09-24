d061cfb546e9ff5946268875da759f98247b517d
RasaanH <Rasaan@resolvve.com>
2024-09-24 19:25:28 -0400

    sets up background

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,6 +5,7 @@ import { View, StyleSheet } from "react-native";
 import { mockMessages } from "@/constants/MockData";
 import { useState } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
+import { AppColors } from "@/constants/Colors";
 
 const navigateToAbout = () => {
   router.navigate("/about");
@@ -20,16 +21,18 @@ export const Chat = () => {
     <TabsProvider defaultIndex={0}>
       <Tabs mode="fixed" disableSwipe={true}>
         <TabScreen label="Conservative" icon="compass">
-          <View></View>
+          <View style={styles.backgroundForChat}></View>
         </TabScreen>
         <TabScreen label="Liberal" icon="bag-suitcase">
-          <GiftedChat
-            messages={messageList}
-            onSend={(messages) => send(messages)}
-            user={{
-              _id: 224687234,
-            }}
-          />
+          <View style={{ backgroundColor: AppColors.DarkGrey, flex: 1 }}>
+            <GiftedChat
+              messages={messageList}
+              onSend={(messages) => send(messages)}
+              user={{
+                _id: 224687234,
+              }}
+            />
+          </View>
         </TabScreen>
       </Tabs>
       <Button style={{ display: "none" }} onPress={() => navigateToAbout()}>
@@ -43,5 +46,9 @@ const styles = StyleSheet.create({
   messageList: {
     display: "flex",
     flexDirection: "column",
+  },
+  backgroundForChat: {
+    // backgroundColor: "#1D3D47",
+    backgroundColor: "blue",
   },
 });
