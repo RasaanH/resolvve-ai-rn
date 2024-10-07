b390ce2214f5a8769dc358e2d52ee29539080bd6
RasaanH <Rasaan@resolvve.com>
2024-10-07 15:31:40 -0400

    signup wip

--- app/signup.tsx
+++ app/signup.tsx
@@ -2,11 +2,30 @@ import { View, StyleSheet } from "react-native";
 import { useState } from "react";
 import { TextInput } from "react-native-paper";
 import { AppColors } from "@/constants/Colors";
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from "./index";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Button } from "react-native-paper";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const auth = getAuth(firebaseApp);
+  const createEmailUser = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("user credential", userCredential);
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("error signing up user with email and password", {
+          errorCode,
+          errorMessage,
+        });
+      });
+  };
   return (
     <View style={styles.background}>
       <TextInput
@@ -20,6 +39,28 @@ export default function SignUp() {
         onChangeText={(text) => setPassword(text)}
         secureTextEntry={true}
       />
+      <TextInput
+        label="Confirm Password"
+        value={confirmPassword}
+        onChangeText={(text) => setConfirmPassword(text)}
+        secureTextEntry={true}
+      />
+      <View>
+        <Button
+          icon="account-plus-outline"
+          mode="elevated"
+          onPress={() => console.log("Pressed")}
+        >
+          Continue
+        </Button>
+        <Button
+          icon="login"
+          mode="elevated"
+          onPress={() => console.log("Pressed")}
+        >
+          Login
+        </Button>
+      </View>
     </View>
   );
 }
