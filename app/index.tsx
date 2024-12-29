965d24634fadec098aa50e3c2fba85ac6672f2ef
RasaanH <Rasaan@resolvve.com>
2024-12-29 17:28:46 -0500

    sets up basic auth logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
 import { Chat } from "../components/chat";
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getAuth } from "firebase/auth";
+import { initializeApp, getApps } from "firebase/app";
+import {
+  initializeAuth,
+  getAuth,
+  signInAnonymously,
+  onAuthStateChanged,
+} from "firebase/auth";
 // @ts-ignore
 import { getReactNativePersistence } from "@firebase/auth/dist/rn/index.js";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
@@ -13,12 +18,34 @@ export const firebaseApp = initializeApp({
   messagingSenderId: "1014320675783",
   appId: "1:1014320675783:web:32d315c275550ad32f77e2",
 });
-let auth;
-if (!auth) {
-  auth = initializeAuth(firebaseApp, {
+
+if (getApps().length === 0) {
+  initializeAuth(firebaseApp, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage),
   });
 }
+
+const auth = getAuth();
+// move this stuff into an auth context
+if (!auth.currentUser?.uid) {
+  signInAnonymously(auth)
+    .then((credentials) => {
+      console.log("anonymous auth credentials", credentials);
+    })
+    .catch((err) => {
+      console.log("error signing in anonymously.", err);
+    });
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const { uid } = user;
+    console.log("successful signin");
+    // ...
+  } else {
+    console.log("At this point, there is no user");
+  }
+});
 
 export default function Index({ navigation }: any) {
   return <Chat />;
