417e3ad2c5e1a959d652f26ea8b1dbdb93ae530d
RasaanH <Rasaan@resolvve.com>
2025-03-10 16:46:19 -0400

    shows paywall

--- app/subscription.tsx
+++ app/subscription.tsx
@@ -1,10 +1,51 @@
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { router } from "expo-router";
+import { useCallback } from "react";
+import { useFocusEffect } from "expo-router";
+import RevenueCatUI, { PAYWALL_RESULT } from "react-native-purchases-ui";
+
+const presentPaywallIfNeeded = async () => {
+  try {
+    // Present paywall for current offering:
+    const paywallResult: PAYWALL_RESULT =
+      await RevenueCatUI.presentPaywallIfNeeded({
+        requiredEntitlementIdentifier: "pro",
+      });
+    console.log(paywallResult);
+    return paywallResult;
+  } catch (err) {
+    console.log("error presenting paywall", err);
+  }
+};
+
+const presentPaywall = async () => {
+  // Present paywall for current offering:
+  const paywallResult = await RevenueCatUI.presentPaywall();
+  // or if you need to present a specific offering:
+
+  switch (paywallResult) {
+    case PAYWALL_RESULT.NOT_PRESENTED:
+    case PAYWALL_RESULT.ERROR:
+    case PAYWALL_RESULT.CANCELLED:
+      return false;
+    case PAYWALL_RESULT.PURCHASED:
+    case PAYWALL_RESULT.RESTORED:
+      return true;
+    default:
+      return false;
+  }
+};
 
 export default function Subscription() {
-  return (
-    <View>
-      <Text>You are now on the subscriptions page</Text>
-    </View>
+  useFocusEffect(
+    // Callback should be wrapped in `React.useCallback` to avoid running the effect too often.
+    useCallback(() => {
+      presentPaywall();
+      router.navigate("/");
+
+      return () => {};
+    }, [])
   );
+
+  return <View></View>;
 }
