6cb21a8775988ffc2095da7b5eb6bcfe7eef818a
Rasaan Hollis <Rasaan@resolvve.com>
2025-06-01 19:32:51 -0400

    adds apple public key

--- constants/purchases.ts
+++ constants/purchases.ts
@@ -1,2 +1,2 @@
 export const public_google_play_key = "goog_acRQJGUnEVpzpqrNTugLKArhfFp";
-export const public_apple_key = "fake_apple_key";
+export const public_apple_key = "appl_rDaPVcNtKtegPmPijnHaYNngtft";
